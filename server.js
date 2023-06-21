64408e509ed8b7bec1aa8a4acaec2ef21831be7e
zchyrlene <112367913+zchyrlene@users.noreply.github.com>
2023-06-21 14:31:48 +0800

    PUT Request

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { Pool } = require('pg');
 const express = require('express');
+const kafka = require('kafka-node');
 const app = express();
 app.use(express.json());
 
@@ -9,6 +10,48 @@ const pool = new Pool({
   host: 'localhost',
   port: 5432,
   database: '3004_db',
+});
+
+const PORT = 8000;
+
+// Kafka Producer setup
+const kafkaClient = new kafka.KafkaClient({ kafkaHost: 'localhost:9092' });
+const producer = new kafka.Producer(kafkaClient);
+
+producer.on('ready', function () {
+  console.log('Kafka Producer is ready');
+});
+
+producer.on('error', function (error) {
+  console.error('Error occurred while initializing Kafka Producer:', error);
+});
+
+app.post('/updateProduct', async (req, res) => {
+  const { productId, name, category, subCategory, stock, price } = req.body;
+
+  try {
+    await updateProductData({ productId, name, category, subCategory, stock, price });
+
+    // Produce a message to Kafka when the product is updated
+    const messagePayload = JSON.stringify({ productId, name, category, subCategory, stock, price });
+    const payload = [
+      { topic: 'product-updates', messages: messagePayload }
+    ];
+
+    producer.send(payload, (error, data) => {
+      if (error) {
+        console.error('Error sending message to Kafka:', error);
+      } else {
+        console.log('Message sent to Kafka:', data);
+      }
+    });
+
+    console.log('Product data updated successfully');
+    res.sendStatus(200);
+  } catch (error) {
+    console.error('Error updating product data:', error);
+    res.sendStatus(500);
+  }
 });
 
 
@@ -57,8 +100,23 @@ app.get('/Sales', async (req, res) => {
   }
 });
 
+// app.put('/updateProduct', async (req, res) => {
+//   const { productId, name, category, subCategory, stock, price } = req.body;
 
-app.listen(8000, () => console.log('Web server is listening on port 8000'));
+//   try {
+//     await updateProductData({ productId, name, category, subCategory, stock, price });
+//     console.log('Product data updated successfully');
+//     res.sendStatus(200);
+//   } catch (error) {
+//     console.error('Error updating product data:', error);
+//     res.sendStatus(500);
+//   }
+// });
+
+
+
+app.listen(PORT, () => console.log(`Web server is listening on port ${PORT}`));
+
 
 async function getProducts() {
   try {
@@ -95,3 +153,75 @@ async function getSales() {
     return [];
   }
 }
+
+// async function updateProductData(productData) {
+//   const { productId, name, category, subCategory, stock, price } = productData;
+
+//   try {
+//     const client = await pool.connect();
+//     const query = `
+//       UPDATE public."Products"
+//       SET "Name" = $1, "Category" = $2, "Sub Category" = $3, "Stock" = $4, "Price" = $5
+//       WHERE product_id = $6;
+//     `;
+//     const values = [name, category, subCategory, stock, price, productId];
+//     await client.query(query, values);
+//     client.release();
+//     console.log('Client released');
+//   } catch (error) {
+//     console.error('Error updating product data:', error);
+//     throw error;
+//   }
+// }
+
+// SQL queries
+const queries = {
+  getProductById: 'SELECT * FROM public."Products" WHERE product_id = $1',
+  updateProduct: 'UPDATE public."Products" SET "Name" = $1, "Category" = $2, "Sub Category" = $3, "Stock" = $4, "Price" = $5 WHERE product_id = $6',
+};
+
+// Route to update a product
+// app.put("/updateItem/", (req, res) => {
+//   const id = parseInt(req.body.id); // Assuming the product ID is provided in the request body
+//   const { name, category, subCategory, stock, price } = req.body;
+
+//   pool.query(queries.getProductById, [id], (error, results) => {
+//     const noProductFound = !results.rows.length;
+//     if (noProductFound) {
+//       res.send("Product does not exist in the database");
+//     } else {
+//       pool.query(
+//         queries.updateProduct,
+//         [name, category, subCategory, stock, price, id],
+//         (error, results) => {
+//           if (error) throw error;
+//           res.status(200).send("Product updated successfully");
+//         }
+//       );
+//     }
+//   });
+// });
+app.put("/updateItem/:id", (req, res) => {
+  const id = parseInt(req.params.id); // Extract the product ID from the URL parameter
+  const { name, category, subCategory, stock, price } = req.body;
+
+  pool.query(queries.getProductById, [id], (error, results) => {
+    const noProductFound = !results.rows.length;
+    if (noProductFound) {
+      res.send("Product does not exist in the database");
+    } else {
+      pool.query(
+        queries.updateProduct,
+        [name, category, subCategory, stock, price, id],
+        (error, results) => {
+          if (error) {
+            console.error("Error updating product:", error);
+            res.status(500).send("Error updating product");
+          } else {
+            res.status(200).send("Product updated successfully");
+          }
+        }
+      );
+    }
+  });
+});
